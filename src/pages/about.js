caeccaddbf215c2d6a1eeb420cb4ef5c30d14902
YashChawla1205 <yashchawla1205@gmail.com>
2025-05-25 21:10:55 +0530

    some changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -50,15 +50,15 @@ const about = () => {
             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About Me</h2>
 
             <p className='font-medium'>
-            Hi, I'm Yash, a Web Developer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field, I am always looking for new and innovative ways to bring my creative visions to life.
+            Hi, I&apos;m Yash, a Web Developer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field, I am always looking for new and innovative ways to bring my creative visions to life.
             </p>
 
             <p className=' my-4 font-medium'>
-            I believe that design is about more than just making things look pretty – it's about solving problems and creating intuitive, enjoyable experiences for users. 
+            I believe that design is about more than just making things look pretty – it&apos;s about solving problems and creating intuitive, enjoyable experiences for users. 
             </p>
 
             <p className='font-medium'>
-            When I'm working on a website or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to further projects.
+            When I&apos;m working on a website or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to further projects.
             </p>
 
             </div>
